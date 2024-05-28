db10614928242358c0bf70e9ae0604e1b70d64db
王亚龙 <>
2024-05-28 23:08:49 +0800

    各个页面配置

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // app.js
-App({})
+App({
+    // 1、绑定生命周期回调函数
+
+    // 2、注册全局数据
+
+    // 3、注册全局方法
+
+    // 4、错误监听
+
+
+    // 生命周期初始化完成
+    onLaunch() {
+        console.log('初始化完成')
+    },
+    // 注册全局数据
+    globalDaata: {
+        name: 'codeMonkey',
+        age: 18
+    },
+    // 注册全局方法
+    sayHello() {
+        console.log('sayHello')
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,16 @@
 // index.js
-Page({})
+Page({
+    // 页面初始化
+    data: {
+        name: "codeMonkey index",
+        age: 100
+    },
+    // 生命周期的监听
+    onLoad() {
+        console.log('index onLoad')
+    },
+    // 自定义处理函数
+    sayHello() {
+        console.log('hello')
+    }
+})
